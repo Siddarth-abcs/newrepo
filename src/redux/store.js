9fc2ce5ede55b163b964793df2dfba85e582acab
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-10 12:56:02 +0530

    frist commit

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import authReducer from "./reducers/authReducer";
+
+const store = createStore(
+    combineReducers({auth: authReducer}),
+    composeWithDevTools(applyMiddleware(thunk))
+)
+export default store;
